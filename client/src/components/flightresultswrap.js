672820f7fbd8a2c9be99eb07cf920dc46358e67c
Juan <71289948+jcintron82@users.noreply.github.com>
2023-03-06 16:15:51 -0500

    Completed the skeleton UI for the flight results page. Merging branch.
    * Added logic to convert carrier codes to full airlines names
    
    * Flight results page skeleton UI complete

--- client/src/components/flightresultswrap.js
+++ client/src/components/flightresultswrap.js
@@ -1,7 +1,11 @@
 import "../css/flightresults.css";
 import { useState } from "react";
 import { queryResponseObj } from "./utility/flightquerymodel";
-
+// var Amadeus = require('amadeus');
+//     var amadeus = new Amadeus({
+//       clientId: 'GRtJGMGPhca4HB7pGdxl8QSEDCdmAMYg',
+//       clientSecret: '3MnGKcngxpSx3KIv'
+//     });
 export function FlightResultsWrap() {
   const [departureLocation, setDepartureLocation] = useState(queryResponseObj[0].message.data);
 //   const [departureTime, setDepartureTime] = useState(queryResponseObj[0].message.data[0].itineraries[0].segments[0].departure.at.slice(11,19));
@@ -15,11 +19,23 @@ const formatFlightTimeString = (string) => {
     const number = Number(string);
     console.log(number)
 };
+//This block is necessary for converting the carrier codes into full names
+//The other option is use the carrier code API but this was the best solution
+for (let i = 0; i < departureLocation.length; i++){
+    for (const [key, value] of Object.entries(queryResponseObj[0].carriers)) {
+        if(departureLocation[i].itineraries[0].segments[0].carrierCode === key)
+        departureLocation[i].itineraries[0].segments[0].carrierCode = value;     
+      }
+}
+
 
   return (
-    <div className="App">
+    <div  className="flightresultspagemainbody">
+        <section className="filterswrap">
+           Filters to apply 
+        </section>
       <article className="flightResultsWrap">
-        <h1>
+
           {departureLocation.map((item, index) => (
             <li
               // onClick={(e) => {
@@ -33,23 +49,29 @@ const formatFlightTimeString = (string) => {
               {" "}
               <article className="flightResultsTabs">
                   <div className="flightmaindetailswrap">
-                    <h1 className="flightTimesWrap">{new Date (item.itineraries[0].segments[0].departure.at).toLocaleTimeString('en-US')} - {new Date (item.itineraries[0].segments[0].arrival.at).toLocaleTimeString('en-US')}
+                  <h3>Carrier: {item.itineraries[0].segments[0].carrierCode}</h3>
+                  <section className="flighttimesmainwrap">
+                    <h1 className="flightTimesWrap">{new Date (item.itineraries[0].segments[0].departure.at).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}<br></br>{'('+item.itineraries[0].segments[0].departure.iataCode+')'}
                      </h1>
-                    <h2>{'('+item.itineraries[0].segments[0].departure.iataCode+')'} -  
-                    {' ('+item.itineraries[0].segments[0].arrival.iataCode+')'} </h2>
-                    <h3>Carrier: {item.itineraries[0].segments[0].carrierCode}</h3>
+                     <article>
+                        Duration and SVG. Direct
+                     </article>
+                    <h1> 
+                   {new Date (item.itineraries[0].segments[0].arrival.at).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}<br></br> {' ('+item.itineraries[0].segments[0].arrival.iataCode+')'} </h1>
+                    </section>
                   </div>
-                  <div>Meta</div>
-                  <div>
-                    Price: {item.price.total} {item.price.currency}<br></br>
-                    Seats left: {item.numberOfBookableSeats}<br></br>
-                  </div>
+                  {/* <div>Meta</div> */}
+                  <section className="flightdetailspricewrap">
+                    <div className="pricewrap">Price: {item.price.total} {item.price.currency}</div>
+                    <div><button className="selectflightbtn">Select</button></div>
+                  </section>
               </article>
             </li>
           ))}
-        </h1>
       </article>
-      <button onClick={log}>Click Me</button>
+      <section className="recommendedwrap">
+           Recommended Flights/Stays
+        </section>
     </div>
   );
 }
--- server/controllers/home.js
+++ server/controllers/home.js
@@ -28,51 +28,74 @@ module.exports = {
       clientSecret: process.env.CLIENT_SECRET
     });
     amadeus.shopping.flightOffersSearch.get({
-        originLocationCode: originLocation,
-        destinationLocationCode: destinationLocation,
-        departureDate: departureDate,
-        returnDate: returnDate,
-        adults: adults,
-        currencyCode:'USD',
-        max: 10,
-        nonStop: true,
-        travelClass: flightClass,
-        maxPrice: maxPrice
-
-        // originLocationCode: 'JFK',
-        // destinationLocationCode: 'LAX',
-        // departureDate: '2023-06-01',
-        // returnDate:  '2023-06-15',
-        // adults: 1,
+        // originLocationCode: originLocation,
+        // destinationLocationCode: destinationLocation,
+        // departureDate: departureDate,
+        // returnDate: returnDate,
+        // adults: adults,
         // currencyCode:'USD',
         // max: 10,
         // nonStop: true,
-        // travelClass: 'BUSINESS',
-        // maxPrice: 5000
+        // travelClass: flightClass,
+        // maxPrice: maxPrice
+        originLocationCode: 'JFK',
+        destinationLocationCode: 'LAX',
+        departureDate: '2023-06-01',
+        returnDate:  '2023-06-15',
+        adults: 1,
+        currencyCode:'USD',
+        max:5,
+        nonStop: true,
+        travelClass: 'BUSINESS',
+        maxPrice: 5000
 
     }).then(function(response){
       response.data = response;
       const data = JSON.parse(response.data.body);
       // confirmFlightPrice(response.data.body.data)
-      return res.json({message:data})
-    }).catch(function(responseError){
+      // console.log(data.dictionaries.carriers)
+
+      // for (let i = 0; i< response.data.body.length;i++){
+      //   amadeus.referenceData.airlines.get({
+      //     airlineCodes : data.data[i].itineraries[i].segments[i].carrierCode,
+      //  }).then((response) => {
+      //   console.log( response.data[0].businessName + 'HI')
+      //   carrierCodes.push(response)
+      //  });
+      // }
+
+  
+        return res.json({message:data, carriers: data.dictionaries.carriers})
+      } )
+    
+      
+    //   for(let i = 0; i < response.length; i++) {      
+        
+    //     amadeus.referenceData.airlines.get({
+    //     airlineCodes : data.data[0].itineraries[0].segments[0].carrierCode,
+    //  }).then((response) => {
+    //   console.log(response)
+    //   code = response
+    //  });}
+
+    .catch(function(responseError){
       console.log(responseError.code + 'HMMMM');
 
     });
-    const confirmFlightPrice = async (data) => {
-      console.log('confirmFlightPrice' + data)
-      const pull = await fetch("https://test.api.amadeus.com/v1/shopping/flight-offers/pricing?include=credit-card-fees&forceClass=false", {
-        body: data,
-        headers: {
-          Accept: "application/vnd.amadeus+json",
-          Authorization: "Bearer r4ZGymi0BGQUApvopt3GwoHUipIl",
-          "Content-Type": "application/vnd.amadeus+json"
-        },
-        method: "POST"
-      });
-      const result = await pull.json();
-      console.log(result)
-    };
+
+    // const confirmFlightPrice = async (data) => {
+    //   console.log('confirmFlightPrice' + data)
+    //   const pull = await fetch("https://test.api.amadeus.com/v1/shopping/flight-offers/pricing?include=credit-card-fees&forceClass=false", {
+    //     body: data,
+    //     headers: {
+    //       Accept: "application/vnd.amadeus+json",
+    //       Authorization: "Bearer r4ZGymi0BGQUApvopt3GwoHUipIl",
+    //       "Content-Type": "application/vnd.amadeus+json"
+    //     },
+    //     method: "POST"
+    //   });
+    //   const result = await pull.json();
+    //   console.log(result)
+    // };
  
-  }
-}
+}}
