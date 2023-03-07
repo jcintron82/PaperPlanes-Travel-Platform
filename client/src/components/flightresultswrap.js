6a45621862519a46937cb164ce6ea48ae8b7d420
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-03-07 15:10:40 -0500

    Added the city search autocomplete API to the search feature

--- client/src/components/flightresultswrap.js
+++ client/src/components/flightresultswrap.js
@@ -1,77 +1,73 @@
 import "../css/flightresults.css";
-import { useState } from "react";
 import { queryResponseObj } from "./utility/flightquerymodel";
-// var Amadeus = require('amadeus');
-//     var amadeus = new Amadeus({
-//       clientId: 'GRtJGMGPhca4HB7pGdxl8QSEDCdmAMYg',
-//       clientSecret: '3MnGKcngxpSx3KIv'
-//     });
+
 export function FlightResultsWrap() {
-  const [departureLocation, setDepartureLocation] = useState(queryResponseObj[0].message.data);
-//   const [departureTime, setDepartureTime] = useState(queryResponseObj[0].message.data[0].itineraries[0].segments[0].departure.at.slice(11,19));
-//   console.log(dapartTime.toLocaleTimeString('en-US'))
-//   const responseArr = [];
-const log = (data) => {
-    setDepartureLocation(data);
-    console.log(queryResponseObj)
-}
-const formatFlightTimeString = (string) => {
-    const number = Number(string);
-    console.log(number)
-};
-//This block is necessary for converting the carrier codes into full names
-//The other option is use the carrier code API but this was the best solution
-for (let i = 0; i < departureLocation.length; i++){
+  const flightsInfo = queryResponseObj[0].message.data;
+
+  //This block is necessary for converting the carrier codes into full names
+  //The other option is use the carrier code API but this was the DRYest solution
+  for (let i = 0; i < flightsInfo.length; i++) {
     for (const [key, value] of Object.entries(queryResponseObj[0].carriers)) {
-        if(departureLocation[i].itineraries[0].segments[0].carrierCode === key)
-        departureLocation[i].itineraries[0].segments[0].carrierCode = value;     
-      }
-}
-
-
+      if (flightsInfo[i].itineraries[0].segments[0].carrierCode === key)
+        flightsInfo[i].itineraries[0].segments[0].carrierCode = value;
+    }
+  }
   return (
-    <div  className="flightresultspagemainbody">
-        <section className="filterswrap">
-           Filters to apply 
-        </section>
+    <div className="flightresultspagemainbody">
+      <section className="filterswrap">Filters to apply</section>
       <article className="flightResultsWrap">
-
-          {departureLocation.map((item, index) => (
-            <li
-              // onClick={(e) => {
-              //   setDeleteIndex(index);
-              // }}
-              // className={
-              //   deleteIndex === index ? "highlightselectedproduct" : "priceli"
-              // }
-              key={index}
-            >
-              {" "}
-              <article className="flightResultsTabs">
-                  <div className="flightmaindetailswrap">
-                  <h3>Carrier: {item.itineraries[0].segments[0].carrierCode}</h3>
-                  <section className="flighttimesmainwrap">
-                    <h1 className="flightTimesWrap">{new Date (item.itineraries[0].segments[0].departure.at).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}<br></br>{'('+item.itineraries[0].segments[0].departure.iataCode+')'}
-                     </h1>
-                     <article>
-                        Duration and SVG. Direct
-                     </article>
-                    <h1> 
-                   {new Date (item.itineraries[0].segments[0].arrival.at).toLocaleTimeString('en-US',{hour:'2-digit', minute:'2-digit'})}<br></br> {' ('+item.itineraries[0].segments[0].arrival.iataCode+')'} </h1>
-                    </section>
-                  </div>
-                  {/* <div>Meta</div> */}
-                  <section className="flightdetailspricewrap">
-                    <div className="pricewrap">Price: {item.price.total} {item.price.currency}</div>
-                    <div><button className="selectflightbtn">Select</button></div>
-                  </section>
-              </article>
-            </li>
-          ))}
+        {flightsInfo.map((item, index) => (
+          <li
+            // className={
+            //   deleteIndex === index ? "highlightselectedproduct" : "priceli"
+            // }
+            key={index}
+          >
+            {" "}
+            <article className="flightResultsTabs">
+              <div className="flightmaindetailswrap">
+                <h3>Carrier: {item.itineraries[0].segments[0].carrierCode}</h3>
+                <section className="flighttimesmainwrap">
+                  <h1 className="flightTimesWrap">
+                    {new Date(
+                      item.itineraries[0].segments[0].departure.at
+                    ).toLocaleTimeString("en-US", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                    <br></br>
+                    {"(" +
+                      item.itineraries[0].segments[0].departure.iataCode +
+                      ")"}
+                  </h1>
+                  <article>Duration and SVG. Direct</article>
+                  <h1>
+                    {new Date(
+                      item.itineraries[0].segments[0].arrival.at
+                    ).toLocaleTimeString("en-US", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                    <br></br>{" "}
+                    {" (" +
+                      item.itineraries[0].segments[0].arrival.iataCode +
+                      ")"}{" "}
+                  </h1>
+                </section>
+              </div>
+              <section className="flightdetailspricewrap">
+                <div className="pricewrap">
+                  Price: {item.price.total} {item.price.currency}
+                </div>
+                <div>
+                  <button className="selectflightbtn">Select</button>
+                </div>
+              </section>
+            </article>
+          </li>
+        ))}
       </article>
-      <section className="recommendedwrap">
-           Recommended Flights/Stays
-        </section>
+      <section className="recommendedwrap">Recommended Flights/Stays</section>
     </div>
   );
 }
--- client/src/components/utility/flightquerymodel.js
+++ client/src/components/utility/flightquerymodel.js
@@ -4,26 +4,42 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 export { FlightSearchModal, queryResponseObj };
 const queryResponseObj = [];
-
+const autocompleteAPIValuesHold = {};
 function FlightSearchModal() {
-  const [departureLocation, setDepartureLocation] = useState([]);
+  const [departureLocation, setDepartureLocation] = useState('');
+  const [arrivalLocation, setArrivalLocation] = useState('');
   const [oneWay, setOneWay] = useState(true);
   const [roundTripSelected, setRoundTrip] = useState(true);
 
   const [queryRecieved, setQueryStatus] = useState();
+
+  const [autocompleteOne, setautocompleteOne] = useState('');
+  const [autocompleteTwo, setautocompleteTwo] = useState('');
+  const [autocompleteThree, setautocompleteThree] = useState('');
+
+  const [autocompleteOneArrival, setautocompleteOneArrival] = useState('');
+  const [autocompleteTwoArrival, setautocompleteTwoArrival] = useState('');
+  const [autocompleteThreeArrival, setautocompleteThreeArrival] = useState('');
+
+
+  const [autocompleteTwoValue, setautocompleteTwoValue] = useState('');
+  const [autocompleteThreeValue, setautocompleteThreeValue] = useState('');
   const navigate = useNavigate();
 
   const body = {
-    departure: "",
+    departure: departureLocation,
     departureDate: "",
-    arrival: "",
+    arrival: arrivalLocation,
     numOfTravelers: 1,
     returnDate: "",
     maxPrice: 5000,
-    flightClass: 'ECONOMY',
+    flightClass: "ECONOMY",
   };
   const flightQuery = async (e) => {
     e.preventDefault();
+    body.departure = body.departure.slice(-3)
+    body.arrival = body.arrival.slice(-3)
+    console.log(body)
     try {
       const pull = await fetch("http://localhost:8000/query", {
         method: "POST",
@@ -42,21 +58,73 @@ function FlightSearchModal() {
     queryResponseObj[0] = data;
     console.log(queryResponseObj);
     navigate("/flightquery");
+    callCitySearchAPI()
+    console.log(process.env.REACT_APP_CLIENT_ID)
     return { message: queryResponseObj };
   };
+
+  const callCitySearchAPI = async (input) => {
+    let token = '';
+    const fetchAuth = await fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
+      body: "grant_type=client_credentials&client_id=" + process.env.REACT_APP_CLIENT_ID+ "&client_secret=" + process.env.REACT_APP_CLIENT_SECRET,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
+      });
+ 
+      const data = await fetchAuth.json()
+      token = data.token_type +' '+ data.access_token
+
+    try {
+      const pull = await fetch("https://test.api.amadeus.com/v1/reference-data/locations?subType=CITY&keyword="+input+"&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL", {
+        headers: {
+          Accept: "application/vnd.amadeus+json",
+          Authorization:token
+        }
+      });
+      const data = await pull.json();
+      // console.log(data);
+      autocompleteAPIValuesHold.options = data;
+      console.log(data)
+
+
+      setautocompleteOne(data.data[0].address.cityName + ', ' +
+      data.data[0].iataCode)
+      setautocompleteTwo(data.data[1].address.cityName + ', ' +
+      data.data[1].iataCode);
+      setautocompleteThree(data.data[2].address.cityName + ', ' +
+      data.data[2].iataCode);
+
+      setautocompleteOneArrival(data.data[0].address.cityName + ', ' +
+      data.data[0].iataCode)
+      setautocompleteTwoArrival(data.data[1].address.cityName + ', ' +
+      data.data[1].iataCode);
+      setautocompleteThreeArrival(data.data[2].address.cityName + ', ' +
+      data.data[2].iataCode);
+
+      // data.data[1].address.stateCode
+      // setautocompleteTwoValue(data.data[1].iataCode);
+      // setautocompleteThreeValue(data.data[2].iataCode);
+
+
+      console.log(data.address.cityName)
+    } catch (err) {
+      // console.log(err);
+    }
+  }
   const selectTripType = (value) => {
     setRoundTrip(value);
   };
-  const recordDepartureLoc = (e,) => {
-    body.departure = e.target.value;
+  const updateSearchParams = (e) => {
+    callCitySearchAPI(e.target.value);
+    setDepartureLocation(e.target.value)
   };
-  const recordDepartureDate = (e) => {
-    body.departureDate = e.target.value;
+  const updateArrivalParams = (e) => {
+    callCitySearchAPI(e.target.value);
+    setArrivalLocation(e.target.value)
   };
-  const recordReturnDate = (e) => {
-    body.returnDate = e.target.value;
-  };
-  const updateSearchParams = (e, valueToUpdate) => {
+  const updateDatesAndFilters = (e, valueToUpdate) => {
     body[valueToUpdate] = e.target.value;
   };
   const updateSearchParamsNum = (e, valueToUpdate) => {
@@ -65,41 +133,60 @@ function FlightSearchModal() {
   return (
     <div className="mainsearchwrap">
       {" "}
-     
-        <form className="flightsearchform">
+      <form className="flightsearchform">
         <section className="flighttypebtnwrap">
-      <button type="button" onClick={(e) => selectTripType(false)}>
-      One-Way
-      </button>
-      <button type="button" onClick={(e) => selectTripType(true)}>
-      Round-Trip
-      </button>
-      </section>
-          <label className="locationinputswrap">
-            <input
-              className="locationinputs"
-              required
-              onChange={(e) => updateSearchParams(e, 'departure')}
-              placeholder="Departing From..."
-            ></input>
-            <input
-              className="locationinputs"
-              required
-              onChange={(e) => updateSearchParams(e, 'arrival')}
-              placeholder="Arriving To..."
-            ></input>
-          </label>
-          <section className="addOnsWrap">
+          <button type="button" onClick={(e) => selectTripType(false)}>
+            One-Way
+          </button>
+          <button type="button" onClick={(e) => selectTripType(true)}>
+            Round-Trip
+          </button>
+        </section>
+        <label className="locationinputswrap">
+          <input
+            autocomplete="off"
+            list="locationslist"
+            id="departure-location-complete"
+            className="locationinputs"
+            required
+            onChange={(e) => updateSearchParams(e, "departure")}
+            placeholder="Departing From..."
+          ></input>
+          <datalist id="locationslist">
+              <option onClick={() => console.log('YO')}  value={autocompleteOne}></option>
+              <option  value={autocompleteTwo}></option>
+              <option  value={autocompleteThree}></option>
+              {/* <option value="Strawberry"></option>
+              <option value="Vanilla"></option> */}
+              </datalist>
+        <input
+            autocomplete="off"
+            list="arrivallist"
+            id="arrival-location-complete"
+            className="locationinputs"
+            required
+            onChange={(e) => updateArrivalParams(e, "arrival")}
+            placeholder="Arriving To..."
+          ></input>
+          <datalist id="arrivallist">
+          <option onClick={() => console.log('YO')}  value={autocompleteOneArrival}></option>
+              <option  value={autocompleteTwoArrival}></option>
+              <option  value={autocompleteThreeArrival}></option>
+              {/* <option value="Strawberry"></option>
+              <option value="Vanilla"></option> */}
+              </datalist>
+        </label>
+        <section className="addOnsWrap">
           {roundTripSelected ? (
             <div className="dateselectionwrap">
               <input
                 required
-                onChange={(e) => updateSearchParams(e, 'departureDate')}
+                onChange={(e) => updateDatesAndFilters(e, "departureDate")}
                 type="date"
               ></input>
               <input
                 required
-                onChange={(e) => updateSearchParams(e, 'returnDate')}
+                onChange={(e) => updateDatesAndFilters(e, "returnDate")}
                 type="date"
               ></input>
             </div>
@@ -107,40 +194,56 @@ function FlightSearchModal() {
             <label className="dateselectionwrap">
               <input
                 required
-                onChange={(e) => updateSearchParams(e, 'departureDate')}
+                onChange={(e) => updateDatesAndFilters(e, "departureDate")}
                 type="date"
               ></input>
             </label>
-          )}<span className="passangerselectwrap">
-          <div className="maxpricewrap">
-            <label className="maxpricelabelwrap">Number of Passengers
-            <select required onChange={(e) => updateSearchParamsNum(e, 'numOfTravelers')}>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={4}>4</option>
-              <option value={5}>5</option>
-            </select></label>
-            <label className="maxpricelabelwrap">Max Price
-              <input onChange={(e) => updateSearchParamsNum(e, 'maxPrice')} placeholder="Max Price?" type="number"></input>
-            </label>
-          </div>
-        </span>
+          )}
+          <span className="passangerselectwrap">
+            <div className="maxpricewrap">
+              <label className="maxpricelabelwrap">
+                Number of Passengers
+                <select
+                  required
+                  onChange={(e) => updateSearchParamsNum(e, "numOfTravelers")}
+                >
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                </select>
+              </label>
+              <label className="maxpricelabelwrap">
+                Max Price
+                <input
+                  onChange={(e) => updateSearchParamsNum(e, "maxPrice")}
+                  placeholder="Max Price?"
+                  type="number"
+                ></input>
+              </label>
+            </div>
+          </span>
         </section>
-        
+
         <section className="flightclasswrap">
-            <label className="maxpricelabelwrap"> Cabin Class
-          <select onChange={(e) => updateSearchParams(e, 'flightClass')} className="flightclassdropdown">
-            <option value='ECONOMY'>Economy</option>
-            <option value='BUSINESS'>Business</option>
-            <option value='FIRST'>First</option>
-          </select>
+          <label className="maxpricelabelwrap">
+            {" "}
+            Cabin Class
+            <select
+              onChange={(e) => updateDatesAndFilters(e, "flightClass")}
+              className="flightclassdropdown"
+            >
+              <option value="ECONOMY">Economy</option>
+              <option value="BUSINESS">Business</option>
+              <option value="FIRST">First</option>
+            </select>
           </label>
-        </section> 
-          <button className="searchBtn" onClick={flightQuery}>
-            Submit
-          </button>
-        </form>
+        </section>
+        <button className="searchBtn" onClick={flightQuery}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
