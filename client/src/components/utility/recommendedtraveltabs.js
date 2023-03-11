7d173e8f07ea3550eb985b60ac64aaa84da272d3
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-03-11 02:46:44 -0500

    Added photos for recommended tabs on homepage

--- client/src/components/utility/recommendedtraveltabs.js
+++ client/src/components/utility/recommendedtraveltabs.js
@@ -1,17 +1,39 @@
 import '../../css/utility/recommendedtabs.css'
-
+//--------------End of module imports---------------//
+import nycphoto from '../../images/home/nyc.avif'
+//--------------End of photo imports---------------//
 export function RecommendedTab({ img, }) {
   return (
+
+<ul className='recommendedtabswrap'>
 <li className="recommendedtabs">
     <div className='imgwrap'>
-        Image
-        {/* <img ></img> */}
+        <img className='recommendedtabsimg' src={nycphoto}></img>
     </div>
     <div className='optionswrap'>
         <p className='textwraps'>City Name</p>
         <p className='textwraps'>Flights | Hotels | Activities</p>
     </div>
 </li>
+<li className="recommendedtabs">
+    <div className='imgwrap'>
+    <img className='recommendedtabsimg' src={nycphoto}></img>
+    </div>
+    <div className='optionswrap'>
+        <p className='textwraps'>City Name</p>
+        <p className='textwraps'>Flights | Hotels | Activities</p>
+    </div>
+</li>
+<li className="recommendedtabs">
+    <div className='imgwrap'>
+    <img className='recommendedtabsimg' src={nycphoto}></img>
+    </div>
+    <div className='optionswrap'>
+        <p className='textwraps'>City Name</p>
+        <p className='textwraps'>Flights | Hotels | Activities</p>
+    </div>
+</li>
+</ul>
   )
 }
 export default RecommendedTab;
