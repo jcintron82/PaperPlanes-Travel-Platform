dfdef3bb59e06b73f39c8a321fad92a87c278c65
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-04-26 17:52:32 -0400

    Debugging vercel deployment

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const cookieParser = require("cookie-parser");
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://paper-planes-travel-platform.vercel.app/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
