96dc78b53ebbe2176bfd12dd86bd8faab572c2b5
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-04-27 13:27:17 -0400

    debugging vercel

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(cors)
 const cookieSession = require("cookie-session")
 const mongoose = require("mongoose");
 const bp = require('body-parser')
